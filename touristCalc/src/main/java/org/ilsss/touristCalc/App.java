49d0ac4f1fdfe91b0a8701227a4730132cc57c07
ilsss <galija1998@rambler.ru>
2019-05-03 01:11:36 +0300

    Первоначальная загрузка проекта

---
+++ touristCalc/src/main/java/org/ilsss/touristCalc/App.java
@@ -0,0 +1,13 @@
+package org.ilsss.touristCalc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
